b39ec64b6a281294aa60b43bfd98169778fab013
Collatty <hakoncb@gmail.com>
2018-10-07 17:19:45 +0200

    Display feedback to user

--- client/src/App.js
+++ client/src/App.js
@@ -11,33 +11,42 @@ class App extends Component {
     }
 
     this.handleClick=this.handleClick.bind(this);
+    this.handleChange=this.handleChange.bind(this);
 
   }
   handleClick () {
     axios.get("/validate", {params: {"socialSecurityNumber": document.getElementById("ssn").value}})
         .then(res => this.setState({status: res.data.status}))
   }
+//In case of continuous validation checkge. Call this on inputs onChange.
+  handleChange() {
+      axios.get("/validate", {params: {"socialSecurityNumber": document.getElementById("ssn").value}})
+          .then(res => this.setState({status: res.data.status}))
+  }
 
   render() {
     return (
       <div id="container">
         <h2 id="header">Sjekk om et fødselsnummer er gyldig her!</h2>
-        <div id="input" className="input-field col s6">
-          <input id="ssn" type="text" className="validate" />
+        <div id="input" className="input-field">
+          <input id="ssn" type="text"/>
           <label className="active">
             Fødselsnummer (11 siffer)
           </label>
-          <button
-            id="button"
-            className="btn waves-effect waves-light"
-            type="submit"
-            name="action"
-            onClick={this.handleClick}
-          >
-            Valider
-            <i className="material-icons right">send</i>
-          </button>
         </div>
+          <button
+              id="button"
+              className="btn waves-effect waves-light"
+              type="submit"
+              name="action"
+              onClick={this.handleClick}
+          >
+              Valider
+              <i className="material-icons right">send</i>
+          </button>
+        <p>
+          {this.state.status}
+        </p>
       </div>
     );
   }
